5600ca68523982066757d0afbd3b8f0840a1c399
jiyonggg <kimvergognoso21@hufs.ac.kr>
2022-07-18 23:13:31 +0900

    Number Button Callback

--- app/handler.py
+++ app/handler.py
@@ -5,40 +5,40 @@ import tkinter.messagebox as msgbox
 # Number Buttons
 def callback_num1():
     '''Button Num1 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="1")
 
 def callback_num2():
     '''Button Num2 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="2")
 
 def callback_num3():
     '''Button Num3 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="3")
 
 def callback_num4():
     '''Button Num4 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="4")
 
 def callback_num5():
     '''Button Num5 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="5")
 
 def callback_num6():
     '''Button Num6 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="6")
 
 def callback_num7():
     '''Button Num7 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="7")
 
 def callback_num8():
     '''Button Num8 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="8")
 
 def callback_num9():
     '''Button Num9 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="9")
 
 def callback_num0():
     '''Button Num0 Callback'''
-    msgbox.showinfo(title="Hello World", message="Hello World!")
+    msgbox.showinfo(title="Number Button Callback", message="0")
